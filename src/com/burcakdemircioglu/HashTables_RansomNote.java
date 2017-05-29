f14e1295889edaf5076e46fe681d117c5bb800ae
Burcak Demircioglu <burcakdemircioglu@gmail.com>
2017-05-29 15:34:50 +0300

    HashTables: Ransom Node question

--- src/com/burcakdemircioglu/HashTables_RansomNote.java
+++ src/com/burcakdemircioglu/HashTables_RansomNote.java
@@ -1,8 +1,48 @@
 package com.burcakdemircioglu;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class HashTables_RansomNote {
 
-    public static void main(String[] args) {
-	// write your code here
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int m = in.nextInt();
+    int n = in.nextInt();
+    String magazine[] = new String[m];
+    for (int magazine_i = 0; magazine_i < m; magazine_i++) {
+      magazine[magazine_i] = in.next();
     }
+    String ransom[] = new String[n];
+    for (int ransom_i = 0; ransom_i < n; ransom_i++) {
+      ransom[ransom_i] = in.next();
+    }
+
+    if (canReplicateRansom(magazine, ransom)) {
+      System.out.println("Yes");
+    } else {
+      System.out.println("No");
+    }
+  }
+
+  public static boolean canReplicateRansom(String magazine[], String ransom[]) {
+    // write your code here
+    HashMap<String, Integer> wordsMap = new HashMap<>();
+    for (String s : magazine) {
+      wordsMap.merge(s, 1, (a, b) -> a + b);
+    }
+
+//    wordsMap.forEach((s, i)-> System.out.println(s+":"+i));
+
+    for (String s : ransom) {
+      Integer existsCount = wordsMap.get(s);
+      if (existsCount == null || existsCount == 0) {
+        return false;
+      } else {
+        wordsMap.merge(s, 1, (a, b) -> a - b);
+      }
+    }
+
+    return true;
+  }
 }
