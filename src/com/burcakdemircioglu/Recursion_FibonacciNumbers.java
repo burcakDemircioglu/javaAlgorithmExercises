4446d30c06a49cbb29c2c214fcacc9e82c18914d
Burcak Demircioglu <burcakdemircioglu@gmail.com>
2017-05-30 09:47:21 +0300

    Recursion: Fibonacci Numbers question

---
+++ src/com/burcakdemircioglu/Recursion_FibonacciNumbers.java
@@ -0,0 +1,30 @@
+package com.burcakdemircioglu;
+
+import java.util.Scanner;
+
+/**
+ * Created by burcakdemircioglu on 30/05/2017.
+ */
+//https://www.hackerrank.com/challenges/ctci-fibonacci-numbers
+//Please do not directly copy the answer! These answers are just for educational purposes.
+public class Recursion_FibonacciNumbers {
+
+  public static int fibonacci(int n) {
+    // Complete the function.
+
+    if (n == 0 || n == 1) {
+      return n;
+    }
+
+    return fibonacci(n - 2) + fibonacci(n - 1);
+
+  }
+
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    scanner.close();
+    System.out.println(fibonacci(n));
+  }
+}
