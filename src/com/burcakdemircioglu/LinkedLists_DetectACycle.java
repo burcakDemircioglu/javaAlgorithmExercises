9d2fc76ab3fefa49cd189aa70b888a3cf7518487
Burcak Demircioglu <burcakdemircioglu@gmail.com>
2017-05-30 09:42:03 +0300

    Linked List: Detect a Cycle question

---
+++ src/com/burcakdemircioglu/LinkedLists_DetectACycle.java
@@ -0,0 +1,36 @@
+package com.burcakdemircioglu;
+
+import java.util.HashMap;
+
+/**
+ * Created by burcakdemircioglu on 30/05/2017.
+ */
+//https://www.hackerrank.com/challenges/ctci-linked-list-cycle
+//Please do not directly copy the answer! These answers are just for educational purposes.
+public class LinkedLists_DetectACycle {
+
+  class Node {
+
+    int data;
+    Node next;
+  }
+
+  boolean hasCycle(Node head) {
+    HashMap<Node, Node> wordsMap = new HashMap<>();
+
+    if (head == null) {
+      return false;
+    }
+
+    while (head != null) {
+      if (wordsMap.get(head) != null) {
+        return true;
+      }
+      wordsMap.put(head, head);
+      head = head.next;
+    }
+
+    return false;
+  }
+
+}
